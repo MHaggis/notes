26a4e5a73a91140bfef93db1da489d4c82675f8b
Michael Haag <5632822+MHaggis@users.noreply.github.com>
2023-01-23 07:31:44 -0700

    Added Pwsh7 Installation

--- utilities/Invoke-SPLPowerShellAuditLogging.ps1
+++ utilities/Invoke-SPLPowerShellAuditLogging.ps1
@@ -67,7 +67,7 @@ function Invoke-SPLPowerShellAuditLogging {
                 param (
                 [Parameter(Mandatory)]
                 [String]
-                [ValidateSet('ScriptBlockLogging','ModuleLogging','TranscriptLogging','EnableAllLogging','DisableAllLogging','ProcessCreateWithCmdline','CreateInputs')]
+                [ValidateSet('ScriptBlockLogging','ModuleLogging','TranscriptLogging','EnableAllLogging','DisableAllLogging','ProcessCreateWithCmdline','CreateInputs','Pwsh7ScriptBlockLogging','Pwsh7ModuleLogging','Pwsh7TranscriptLogging','InstallPwSh7')]
                 $method,
         
                 [Parameter()]
@@ -105,6 +105,117 @@ function Invoke-SPLPowerShellAuditLogging {
 "@
 
         $ascii
+
+
+        function Invoke-SPLPS7ScriptBlockLogging {
+
+                Write-Host "Enabling PwSh 7 Script Block Logging"   
+
+                $registryPath = "HKLM:\Software\Policies\Microsoft\PowerShellCore\ScriptBlockLogging"
+                $Name = "EnableScriptBlockLogging"
+                $value = "1"
+                
+                IF (!(Test-Path $registryPath)) {
+                        New-Item -Path $registryPath -Force | Out-Null
+                        New-ItemProperty -Path $registryPath -Name $name -Value $value -PropertyType DWORD -Force | Out-Null
+                } ELSE {
+                        New-ItemProperty -Path $registryPath -Name $name -Value $value -PropertyType DWORD -Force | Out-Null
+                }
+                
+                $registryPath = "HKLM:\Software\Policies\Microsoft\PowerShellCore\ScriptBlockLogging"
+                $Name = "EnableScriptBlockInvocationLogging"
+                $value = "1"
+                
+                IF (!(Test-Path $registryPath)) {
+                        New-Item -Path $registryPath -Force | Out-Null
+                        New-ItemProperty -Path $registryPath -Name $name -Value $value -PropertyType DWORD -Force | Out-Null
+                } ELSE {
+                        New-ItemProperty -Path $registryPath -Name $name -Value $value -PropertyType DWORD -Force | Out-Null
+                }
+                
+        }
+
+        function Invoke-SPLPS7ModuleLogging {
+
+                Write-Host "Enabling PowerShell Module Logging"
+
+                $registryPath = "HKLM:\Software\Policies\Microsoft\PowerShellCore\ModuleLogging"
+                $Name = "EnableModuleLogging"
+                $value = "1"
+                
+                IF (!(Test-Path $registryPath)) {
+                        New-Item -Path $registryPath -Force | Out-Null
+                        New-ItemProperty -Path $registryPath -Name $name -Value $value -PropertyType DWORD -Force | Out-Null
+                } ELSE {
+                        New-ItemProperty -Path $registryPath -Name $name -Value $value -PropertyType DWORD -Force | Out-Null
+                }
+                
+                $registryPath = "HKLM:\Software\Policies\Microsoft\PowerShellCore\ModuleLogging"
+                $Name = "*"
+                $value = "*"
+                
+                IF (!(Test-Path $registryPath)) {
+                        New-Item -Path $registryPath -Force | Out-Null
+                        New-ItemProperty -Path $registryPath -Name $name -Value $value -Force | Out-Null
+                } ELSE {
+                        New-ItemProperty -Path $registryPath -Name $name -Value $value -Force | Out-Null
+                }
+        
+        }
+
+        function Invoke-SPLPS7TranscriptLogging {
+
+                Write-Host "Enabling PowerShell 7 Transcript Logging"
+
+                $registryPath = "HKLM:\Software\Policies\Microsoft\PowerShellCore\Transcription"
+                $Name = "EnableInvocationHeader"
+                $value = "1"
+                
+                IF (!(Test-Path $registryPath)) {
+                        New-Item -Path $registryPath -Force | Out-Null
+                        New-ItemProperty -Path $registryPath -Name $name -Value $value -PropertyType DWORD -Force | Out-Null
+                } ELSE {
+                        New-ItemProperty -Path $registryPath -Name $name -Value $value -PropertyType DWORD -Force | Out-Null
+                }
+                
+                $registryPath = "HKLM:\Software\Policies\Microsoft\PowerShellCore\Transcription"
+                $Name = "EnableTranscripting"
+                $value = "1"
+                
+                IF (!(Test-Path $registryPath)) {
+                        New-Item -Path $registryPath -Force | Out-Null
+                        New-ItemProperty -Path $registryPath -Name $name -Value $value -PropertyType DWORD -Force | Out-Null
+                } ELSE {
+                        New-ItemProperty -Path $registryPath -Name $name -Value $value -PropertyType DWORD -Force | Out-Null
+                }
+                
+                $registryPath = "HKLM:\Software\Policies\Microsoft\PowerShellCore\Transcription"
+                $Name = "OutputDirectory"
+                
+                $value = $TransactionLogPath
+                
+                IF (!(Test-Path $TransactionLogPath)) {
+                        New-Item -Path $TransactionLogPath  -ItemType Directory -Force | Out-Null
+                } ELSE {
+                        Write-Host "Unable to create directory $TransactionLogPath"
+                }
+
+                IF (!(Test-Path $registryPath)) {
+                        New-Item -Path $registryPath -Force | Out-Null
+                        New-ItemProperty -Path $registryPath -Name $name -Value $value -Force | Out-Null
+                } ELSE {
+                        New-ItemProperty -Path $registryPath -Name $name -Value $value -Force | Out-Null
+                }
+        
+        }
+
+        function Get-PwSh7 {
+                Write-Host "Installing PwSh7 - Prepare for a prompt"   
+                Invoke-Expression "& { $(Invoke-RestMethod https://aka.ms/install-powershell.ps1) } -UseMSI"
+                # run ps1 scripts from pw7 path to install admx and register.
+                & 'C:\Program Files\PowerShell\7\RegisterManifest.ps1'
+                & 'C:\Program Files\PowerShell\7\InstallPSCorePolicyDefinitions.ps1'
+        }
 
         function Invoke-SPLScriptBlockLogging {
 
@@ -233,6 +344,7 @@ function Invoke-SPLPowerShellAuditLogging {
                 Invoke-SPLModuleLogging
                 Invoke-SPLTranscriptLogging
                 Invoke-SPLProcessCreationIncludeCmdLine
+                Invoke-SPLPS7ScriptBlockLogging
                 Write-Host "All Logging is Enabled. May the force be with you." -ForegroundColor Green
         }
         
@@ -244,6 +356,12 @@ renderXml = 1
 disabled = false
 index = win 
 
+[WinEventLog://PowerShellCore/Operational]
+index=win
+sourcetype=XmlWinEventLog:Microsoft-Windows-PowerShellCore/Operational
+disabled = false
+renderXml = 1
+
 [monitor://$TransactionLogPath]
 sourcetype = powershell:transcript
 disabled = false
@@ -252,18 +370,18 @@ time_before_close = 300
 index = win
 "@
 
-                $InputsConfFile = "$InputsFilePath\SPLAuditLogging\local\inputs.conf"
+        $InputsConfFile = "$InputsFilePath\SPLAuditLogging\local\inputs.conf"
 
-                IF (!(Test-Path $InputsConfFile)) {
-                        new-item -Path $InputsFilePath\SPLAuditLogging\local\ -itemtype directory -Force
-                        new-item -Path $InputsConfFile -ItemType File -Force
-                        Add-Content -Path $InputsConfFile -Value $InputsConf -Force -WarningAction Ignore
-                        Write-Host "Restarting SplunkForwarder" -ForegroundColor Green
-                        Restart-Service SplunkForwarder -Force
-                        Write-Host "$InputsConfFile has been created and SplunkForwarder restarted." -ForegroundColor Green
-                } ELSE {
-                        Write-Host "The $InputsConfFile is already created." -ForegroundColor Red
-                }
+        IF (!(Test-Path $InputsConfFile)) {
+                new-item -Path $InputsFilePath\SPLAuditLogging\local\ -itemtype directory -Force
+                new-item -Path $InputsConfFile -ItemType File -Force
+                Add-Content -Path $InputsConfFile -Value $InputsConf -Force -WarningAction Ignore
+                Write-Host "Restarting SplunkForwarder" -ForegroundColor Green
+                Restart-Service SplunkForwarder -Force
+                Write-Host "$InputsConfFile has been created and SplunkForwarder restarted." -ForegroundColor Green
+        } ELSE {
+                Write-Host "The $InputsConfFile is already created." -ForegroundColor Red
+        }
 
         }
 
@@ -360,6 +478,10 @@ index = win
                 'DisableAllLogging' { Invoke-SPLDisableAllLogging }
                 'ProcessCreateWithCmdline' { Invoke-SPLProcessCreationIncludeCmdLine }
                 'CreateInputs' { Invoke-SPLInputs }
+                'Pwsh7ScriptBlockLogging' { Invoke-SPLPS7ScriptBlockLogging }
+                'Pwsh7ModuleLogging' {Invoke-SPLPS7ModuleLogging}
+                'Pwsh7TranscriptLogging' {Invoke-SPLPS7TranscriptLogging}
+                'InstallPwSh7' {Get-PwSh7}
                 }
         
                 
